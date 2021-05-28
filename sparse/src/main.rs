ab324b24a4010513fa98f69c8ad6b11bed1a9631
Yomguithereal <guillaumeplique@gmail.com>
2021-05-28 15:01:30 +0200

    Working test

--- sparse/src/main.rs
+++ sparse/src/main.rs
@@ -11,10 +11,15 @@ struct Record {
     weights: String
 }
 
+type SparseVector = HashMap<u32, f64>;
+
 const LIMIT: usize = 10_000;
 // const LIMIT: usize = usize::MAX;
 
-fn sparse_dot_product_distance(first: &HashMap<u32, f64>, second: &HashMap<u32, f64>) -> f64 {
+const THRESHOLD: f64 = 0.69;
+const WINDOW: usize = 1_500_000;
+
+fn sparse_dot_product_distance(first: &SparseVector, second: &SparseVector) -> f64 {
     let mut shortest = first;
     let mut longest = second;
 
@@ -36,19 +41,21 @@ fn sparse_dot_product_distance(first: &HashMap<u32, f64>, second: &HashMap<u32, 
     return 1.0 - product;
 }
 
-fn clustering() -> Result<(), Box<dyn Error>> {
+fn clustering<'a>() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_path("../data/vectors.csv")?;
     let mut i = 0;
 
     let bar = ProgressBar::new(7_000_000);
 
     let mut index: HashMap<u32, VecDeque<usize>> = HashMap::new();
+    let mut vectors: HashMap<usize, SparseVector> = HashMap::new();
+    let mut nearest_neighbors: Vec<(usize, f64)> = Vec::new();
 
     for result in rdr.deserialize() {
         if i >= LIMIT {
             break;
         }
-        i += 1;
+
         if i % 10_000 == 0 {
             bar.inc(10_000);
         }
@@ -57,9 +64,11 @@ fn clustering() -> Result<(), Box<dyn Error>> {
 
         // println!("{:?}", record);
 
-        let mut sparse_vector: HashMap<u32, f64> = HashMap::new();
+        let mut sparse_vector: SparseVector = HashMap::new();
 
         if record.dimensions.is_empty() {
+            nearest_neighbors.push((i, 0.0));
+            i += 1;
             continue;
         }
 
@@ -94,8 +103,60 @@ fn clustering() -> Result<(), Box<dyn Error>> {
         }
 
         // println!("{:?}", candidates.len());
+
+        // Finding the nearest neighbor
+        let mut best_distance = 2.0;
+        let mut best_candidate: Option<usize> = None;
+
+        for candidate in candidates.iter() {
+            let other_sparse_vector = vectors.get(candidate).unwrap();
+
+            let d = sparse_dot_product_distance(&sparse_vector, other_sparse_vector);
+
+            // println!("{:?}", d);
+
+            if d > THRESHOLD {
+                continue;
+            }
+
+            if d < best_distance {
+                best_distance = d;
+                best_candidate = Some(*candidate);
+            }
+        }
+
+        match best_candidate {
+            Some(best_candidate_index) => {
+                nearest_neighbors.push((best_candidate_index, best_distance));
+            }
+            None => {
+                nearest_neighbors.push((i, 0.0));
+            }
+        }
+
+        vectors.insert(i, sparse_vector.to_owned());
+
+        // T.append(t1)
+
+        // if len(T) > window:
+        //     t3 = T.popleft()
+
+        //     for dim in t3['vector'].keys():
+        //         I[dim].popleft()
+        i += 1;
     }
+
     bar.finish();
+
+    let with_nearest_neighbor: Vec<(usize, f64)> = nearest_neighbors
+        .into_iter()
+        .enumerate()
+        .filter(|(j, c)| j != &c.0)
+        .map(|(_, c)| c)
+        .collect();
+
+    println!("{:?}, {:?}", with_nearest_neighbor, with_nearest_neighbor.len());
+
     Ok(())
 }
 
