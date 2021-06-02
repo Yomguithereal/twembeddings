dc37d812d2d0c3fda5397e32634bce01ff577fa0
Yomguithereal <guillaumeplique@gmail.com>
2021-06-02 11:38:35 +0200

    Link

--- sparse/src/main.rs
+++ sparse/src/main.rs
@@ -48,6 +48,7 @@ fn sparse_dot_product_distance(helper: &SparseSet<f64>, other: &[(usize, f64)]) 
 // TODO: sanity tests
 // TODO: start from window directly to easy test
 // TODO: use #[cfg] for stats within the function
+// TODO: https://dash.harvard.edu/bitstream/handle/1/38811431/GHOCHE-SENIORTHESIS-2016.pdf?sequence=3
 fn clustering() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_path("../data/vectors.csv")?;
     let mut i = 0;
